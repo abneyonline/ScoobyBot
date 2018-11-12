faa3e9e2e6323af8a13d1b5993757bdf75417f6c
Elijah Abney <elijah@bney.us>
2018-11-12 18:18:14 -0500

    Established initial minimum viable product. On run will send one (1) tweet from the supplied lexicon

--- scoobert.py
+++ scoobert.py
@@ -1,14 +1,41 @@
 import cv2 as cv
+import tweepy
+import os
 from numpy import sqrt, log, cos, pi, random
 
-cap = cv.VideoCapture('newbert.mp4')
+# Set Up Media to Pull From
+seriesMap = {'WhereAreYou':'Scooby-Doo Where are You!',
+'ScoobyDooShow':'The Scooby-Doo Show'}
+lexicon = os.environ['SCOOBYLEXICON']
 
+# Determine Exact Episode
+series = random.choice(os.listdir(lexicon))
+episode = random.choice(os.listdir(os.path.join(lexicon, series)))
+fullPath = os.path.join(lexicon, series, episode)
+# fullPath = os.path.join(lexicon, 'WhereAreYou', 'S01E01 - What A Night For A Knight.mp4')
+
+# Open video file
+cap = cv.VideoCapture(fullPath)
+
+# Find the target frame
 length = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
 targetFrame = random.randint(0, length-1)
+
+# Retrieve the target frame
 cap.set(cv.CAP_PROP_POS_FRAMES, targetFrame)
 ret, frame = cap.read()
 gray = cv.cvtColor(frame, 0)
+# Print the target frame to file
 cv.imwrite('temp.png',gray)
+# Cleanup
 cap.release()
 cv.destroyAllWindows()
 
+# Retrieve @BotScooby's oauth information from environment variables.
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
+
+api = tweepy.API(auth)
+
+# print(seriesMap[series] + " " + episode[:-4])
+api.update_with_media('temp.png', seriesMap[series] + " " + episode[:-4])
